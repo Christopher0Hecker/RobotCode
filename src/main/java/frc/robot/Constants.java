e3a2baa56707f56eebc67f707b1eb8c3ed5a1324
Christopher0Hecker <149629620+Christopher0Hecker@users.noreply.github.com>
2023-11-08 18:30:51 -0500

    Finished Constants, Robot, and Wheels

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,16 +1,26 @@
 package frc.robot;
 
+import frc.robot.Constants.CANIds;
+
 public final class Constants {
     
     public static class CANIds 
     {
-
+        public static final int leftWheelMotor = 1;
+        public static final int rightWheelMotor = 2;
     }
     public static class DIO 
     {
+        public static int WheelLimitSwitch = 1;
+    }
 
+    public static class Wheels
+    {
+        public static int leftWheelMotor = CANIds.leftWheelMotor;
+        public static int rightWheelMotor =CANIds.rightWheelMotor;
+
+        public static double WheelForwardSpeed = 1;
+        public static double WheelBackwardSpeed = -1;
     }
 }
-
-
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@ package frc.robot;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.Wheels;
 
 public class Robot extends TimedRobot 
 {
@@ -11,7 +12,7 @@ public class Robot extends TimedRobot
   public static RobotContainer m_robotContainer;
 
   //Subsystems
-
+  
   //Navx
   public static AHRS m_ahrs;
 
@@ -20,7 +21,7 @@ public class Robot extends TimedRobot
   {
 
     //Subsystems
-    
+    Wheels wheels = new Wheels();
     //Container
     m_robotContainer = new RobotContainer();
 
